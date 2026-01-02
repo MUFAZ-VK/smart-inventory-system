30e8124032a0777ec2f49c07f648b515d3c3313a
MUFAZ VK <mufazacer@gmail.com>
2026-01-02 22:49:30 +0530

    Update urls.py

--- backend/inventory_system/urls.py
+++ backend/inventory_system/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 # Main URL configuration for the Django project
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin panel
     path('api/', include('inventory.urls')),  # Include all inventory API endpoints
+    # Serve React Frontend for any other route
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
